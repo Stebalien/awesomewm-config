2ce8f4aeb5d68ded75bbf0e847a757aa3a990112
Steven Allen <steven@stebalien.com>
2011-12-25 11:56:30 -0800

    Fix calendar's next/prev year.

--- cal.sh
+++ cal.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-year=`date +%y`
+year=`date +%Y`
 month=`date +%m`
 day=`date +%_d`
 
@@ -9,14 +9,20 @@ case $month in
     1)
         pmonth=12
         nmonth=2
+        pyear=$(($year-1))
+        nyear=$year
         ;;
     12)
         pmonth=11
         nmonth=1
+        pyear=$year
+        nyear=$(($year+1))
         ;;
     *)
         pmonth=$(($month-1))
         nmonth=$(($month+1))
+        pyear=$year
+        nyear=$year
         ;;
 esac
 
@@ -30,8 +36,8 @@ format() {
     done
 }
 
-paste -d ' ' <(cal $pmonth $year | format "<span color=\"$color\">" "</span>") \
+paste -d ' ' <(cal $pmonth $pyear | format "<span color=\"$color\">" "</span>") \
       <(cal | format "" "" | sed "1!s:$day\b:<span color=\"$color2\">$day</span>:") \
-      <(cal $nmonth $year | format "<span color=\"$color\">" "</span>") | head -n-1
+      <(cal $nmonth $nyear | format "<span color=\"$color\">" "</span>") | head -n-1
 echo "<span color=\"$color\">_________________________________________________________________</span>"
 rem | tail -n+2 | fold -s -64
